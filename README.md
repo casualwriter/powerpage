ac67f8fc1374508595bddd064c1a7a3d8c7d81c9
HUNG CHONG KI - Information Systems Ckhung Technology <ckhung@AHHK.LOCAL>
2021-05-07 15:55:39 +0800

    update readme

--- README.md
+++ README.md
@@ -12,12 +12,37 @@ OLE web-browser with new protocol pb:// or ps:// to provide below features
 * (PB) Call Powerbuilder Windows/Functions 
 * (Misc) Variables, session information 
 
-
 ### Files and Installation
 
-Simply download from "release" folder
+* Simply download from "release" folder, and unzip the file
+* Source code, and latest version can be downloaded from "source" folder
+
+### API and Interface
+
+API call provide in pb:// protocol command, or javascript ``pb`` funciton call. 
+
+#### Global Features (Callback, Prompt, @JsVar, Secured protocol)  
+
+Description | pb protocol | javascript
+------------|-------------|-----------------
+Prompt for confirmation, then run command | pb://?Open notepad?/run/notepad.exe | pb.confirm('Open notepad').run('notepad.exe')  
+Callback js function after run command | pb://callback/mycallback/run/calc.exe | pb.run('calc.exe','mycallback')  
+
+#### Run / RunAt / Shell
+
+#### File Accessibility
+
+#### Database Accessibility
+
+#### Work with Powerbuilder
+
+#### Session / Global Variables
+
+#### Url Navigation / Misc
 
 ### Modification History
+
+* 2021/05/07, beta version, v0.41 
 
 ### Lisense
 
---
+++ source/powerpage.js
@@ -0,0 +1,131 @@
+//========================================================================
+// PB interface. 
+// 20210301. ck.  pb.callback(), run commands, and all basic features
+// 20210505. ck.  pb.session()
+// 20210507. ck.  pb.console(), pb.eval() for console support
+//========================================================================
+// pb main function, pb('varname') = js.varname, pb('#div') = getElementById
+var pb = function (n) { return n[0]=='#'? document.getElementById(n.substr(2)) : window[n]; }
+
+//=== show message for microhelp, internal error 
+pb.microhelp = function (msg) { document.title='pb://microhelp/'+ msg } 
+pb.error = function(code,msg) { pb.microhelp( '[error='+ code +'] ' + msg ) }
+
+//=== console support 
+if (typeof console=="undefined") var console = { }
+
+console.log = pb.console = function () {
+  var i, msg = ''
+  for (i=0; i < arguments.length; i++) {
+    if (typeof arguments[i] === "object") {
+      msg += (i==0?'':', ') + JSON.stringify(arguments[i])
+    } else {  
+      msg += (i==0?'':', ') + arguments[i]
+    }
+  }
+  pb.microhelp( '> ' + msg )   
+}
+
+pb.eval = function (exp) {
+  try {
+    var result = eval(exp)
+    pb.microhelp( exp  + (typeof result=="undefined"? '' : '|n= ' + (typeof result=='object'? JSON.stringify(result) : result ) ) )
+  } catch (e) {
+    pb.microhelp( exp  + '|nError: ' + e.message )      
+  }  
+}
+
+//=== secure protocol support
+pb.cmd = { protocol:'pb://' }
+pb.secure = function() { pb.cmd.protocol = 'ps://' ; return pb }
+
+//=== callback function for Powerbuilder
+pb.callback = function ( name, result, type, url ) {
+  if (typeof window[name] === "function") {
+      window[name]( result, type, url );
+  } else if (name) {
+      alert( 'callback function ' + name + '() not found!\n\n type:' + type + ' from url: ' + url 
+             + '\n function: '+name + '\n Result: \n\n' + result )
+  } else if (type=='json'||type=='table'||type=='sql'||type=='file') {
+      alert( 'callback (default)\n\n type:' + type + ' from url: ' + url 
+             + '\n function: '+name + '\n Result: \n\n' + result )
+  }  
+}
+
+//=== add Prompt to parepare queue
+pb.prompt = pb.confirm = function (msg) { 
+  pb.cmd.prompt = msg; 
+  return pb 
+}
+ 
+//=== prepare command prefix with Prompt and callback
+pb.cmd.prepare = function(callback) {
+   pb.cmd.prefix = pb.cmd.protocol 
+   if (pb.cmd.prompt) {
+     pb.cmd.prefix += '?' + pb.cmd.prompt + '?/'
+     pb.cmd.prompt = null  
+   }
+   
+   pb.cmd.prefix += (callback? 'callback/'+callback+'/' : '')
+   return pb.cmd.prefix
+}
+
+//=== generate parameter string
+pb.cmd.parameters = function(args) {
+  if (!args) {
+    return ''
+  } else if (typeof args === "string" || typeof args === "number") {
+    return "/" + args
+  } else if (typeof args === "object") {
+    var i, key='/';
+    for (i=0; i<args.length; i++) { key += args[i]; }
+    return key;
+  } else {  
+    pb.error( '01', "[error] unknow type of arguments! " + args )
+  } 
+}
+
+//=== submit command to Powerbuilder. (support cmd history later)
+pb.submit = function (cmd) {
+  window.location = pb.cmd.command = cmd
+}
+
+//=== call run() commands
+pb.run = function ( cmd, callback ) { pb.submit( pb.cmd.prepare(callback) + 'run/' + cmd ) }
+pb.runat = function ( cmd, callback ) { pb.submit( pb.cmd.prepare(callback) + 'run@/' + cmd ) }   
+
+//=== shell functions
+pb.shell = { name: 'shell functions' }
+pb.shell.open = function (path,callback) { pb.submit( pb.cmd.prepare(callback) + 'shell/open/' + path ) };
+pb.shell.max  = function (path,callback) { pb.submit( pb.cmd.prepare(callback) + 'shell/max/' + path ) };
+pb.shell.print = function (path,callback) { pb.submit( pb.cmd.prepare(callback) + 'shell/print/' + path ) }; 
+pb.shell.run = function (path,callback) { pb.submit( pb.cmd.prepare(callback) + 'shell/run/' + path ) };
+
+//=== call Powerbuilder dialog, window, function
+pb.dialog = function (win, args, callback) { pb.submit( pb.cmd.prepare(callback) + 'dialog/' + win + pb.cmd.parameters(args) ) }
+pb.window = function (win, args, callback) { pb.submit( pb.cmd.prepare(callback) + 'window/' + win + pb.cmd.parameters(args) ) }
+pb.function = function (win, args, callback) { pb.submit( pb.cmd.prepare(callback) + 'function/' + win + pb.cmd.parameters(args) ) }
+pb.popup = function (url,callback) { pb.submit( pb.cmd.prepare(callback) + 'popup/' + url ) }
+
+//=== database function
+pb.db = { name: 'shell functions' }
+pb.db.json  = function ( sql, callback ) { pb.submit( pb.cmd.prepare(callback) + 'json/' + sql ) };
+pb.db.table = pb.db.html  = function ( sql, callback ) { pb.submit( pb.cmd.prepare(callback) + 'table/' + sql ) };
+pb.db.query = pb.db.select = function ( sql, callback ) { pb.submit( pb.cmd.prepare(callback) + 'json/' + sql ) };  
+pb.db.execute = pb.db.update = function ( sql, callback ) { pb.submit( pb.cmd.prepare(callback) + 'sql/execute/' + sql ) };
+pb.db.confirm = pb.db.prompt = function ( sql, callback ) { pb.submit( pb.cmd.prepare(callback) + 'sql/prompt/' + sql ) };
+
+//==== pb session. 
+// session(name) -> get value
+// session(name,value) -> set value and sync to pb
+// session(name,value,'init') -> call from pb, init value (no call back)    
+pb.session = function ( name, value, from ) {
+   if (arguments.length == 1) {
+      return pb.session[name]
+   } else if (arguments.length == 2) {
+      pb.session[name] = (value.substr(0,1)=='{'? JSON.parse(value) : value )
+      window.location='pb://session/'+ name + '/' + value;
+   } else {
+      pb.session[name] = (value.substr(0,1)=='{'? JSON.parse(value) : value )
+   }
+}
